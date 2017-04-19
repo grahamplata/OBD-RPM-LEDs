62ba5231e63b229c45c8b2ff25f358faacee930d
Graham Plata <graham.plata@gmail.com>
2017-04-18 23:28:33 -0400

    rpms intervals set leds

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ options.baudrate = 115200;
 var serialOBDReader = new OBDReader("/dev/rfcomm0", options);
 var dataReceivedMarker = {};
 
+// Express Server setup
+var app = express();
 
-// OBD connection setup
-// Don't set the bluetooth-obd during development
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(3000);
+console.log('Server listening on port 3000');
+
+// OBDII Calls
 if (process.env.NODE_ENV != "development") {
 
     serialOBDReader.on('dataReceived', function (data) {
@@ -24,40 +30,65 @@ if (process.env.NODE_ENV != "development") {
         dataReceivedMarker = data;
     });
 
+    // On connection begin polling data
     serialOBDReader.on('connected', function (data) {
         this.addPoller("vss");
         this.addPoller("rpm");
         this.addPoller("temp");
 
-        this.startPolling(); // 75 * 3 == .225seconds polling rate
+        this.startPolling(); // 75ms default polling rate * 3 for each call == .225seconds polling rate
     });
 
     serialOBDReader.connect();
 }
 
+// Socket.IO 
+io.on('connection', function (socket) {
+    console.log('New client connected!');
+    var delayMillis = 100; //1000 = 1 second
+
+    //send data to client
+    setInterval(function () {
+
+        // Change values so you can see it go up when developing
+        if (process.env.NODE_ENV === "development") {
+            if (rpm < 7200) {
+                rpm += 11
+            } else {
+                rpm = 0
+            }
+        }
+    }, delayMillis);
+});
+
+
 // Simulates RPMS on Blink
 if (process.env.NODE_ENV === "development") {
     var delayMillis = 50; //1000 = 1 second
     var num = 0; //init number variable
-
+    var rpm = 0; //rpms
     //sets interval at which leds are sequentially lit
     setInterval(function () {
-        if (num < 8) {
-            leds.simulate_rpm(num);
-            //console.log('pixel: ' + num + ' set');
-            num += 1
+        if (rpm < 7200) {
+            Set_LEDS(rpm);
+            rpm += 11
+            //console.log(rpm);
         } else {
-            num = 0 //reset loop
+            Set_LEDS(rpm);
+            rpm = 0
         }
-    }, delayMillis); //set delay
+    }, delayMillis);
+}
+// converts rpms to led values
+function Convert_RPM_to_LED(rpm) {
+    var led_num = rpm / 1000
+    return Math.round(led_num);
 }
 
-
-// Express
-// Server setup
-var app = express();
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-var server = app.listen(3000);
-console.log('Server listening on port 3000');
+// sets leds based on rpms
+function Set_LEDS(rpm) {
+    var num = Convert_RPM_to_LED(rpm);
+    if (num < 8) {
+        leds.simulate_rpm(num);
+    }
+}
